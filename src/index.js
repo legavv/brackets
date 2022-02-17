e1e2e9037f0eef046665d3a439e74a3ce027e9a1
Vadim Lega <legavv@mail.ru>
2022-02-17 21:52:43 +0300

    solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const arrStr = str.split('');
+    let res = trimSimilarBrackets();
+
+    function trimSimilarBrackets () {
+        let pair = [];
+        arrStr.forEach((el, index) => {
+            pair.push(el)
+            if (index > 0) {
+                bracketsConfig.forEach((item) => {
+                    if (pair.join('') === item.join('')) {
+                        arrStr.splice(index - 1, 2);
+                        trimSimilarBrackets();
+                    }
+                })
+                pair.shift(); 
+            }
+        })
+        return arrStr;
+    }
+    return res.length === 0;
 }
